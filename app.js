25c841e88b3d7e6825ea9d944039d6b5e234d72e
Tristin-Young <86166175+Tristin-Young@users.noreply.github.com>
2023-01-24 23:42:25 -0500

    Style Updates - Color name broken

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-// import TinyColor.js
-import tinycolor from 'tinycolor2';
 
+window.onload = function(){
 // Get image input and canvas elements
 const imageInput = document.getElementById('image-input');
 const imageCanvas = document.getElementById('image-canvas');
@@ -9,31 +8,46 @@ const colorOutput = document.getElementById('color-output');
 // Create a canvas context
 const ctx = imageCanvas.getContext('2d');
 
+function rgbToHex(r, g, b) {
+  return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+}
+
 // Add event listener to the image input
 imageInput.addEventListener('change', (e) => {
-    // Get selected image file
-    const imageFile = e.target.files[0];
+  // Get selected image file
+  const imageFile = e.target.files[0];
 
-    // Create an image object
-    const image = new Image();
-    image.src = URL.createObjectURL(imageFile);
+  // Create an image object
+  const image = new Image();
+  image.src = URL.createObjectURL(imageFile);
 
-    // Draw image on the canvas
-    image.onload = () => {
-        ctx.drawImage(image, 0, 0, imageCanvas.width, imageCanvas.height);
-    };
+  // Draw image on the canvas
+  image.onload = () => {
+    ctx.drawImage(image, 0, 0, imageCanvas.width, imageCanvas.height);
+  };
 });
 
 // Add event listener to the canvas
 imageCanvas.addEventListener('click', (e) => {
-    // Get the x and y coordinates of the click event
-    const x = e.clientX - imageCanvas.offsetLeft;
-    const y = e.clientY - imageCanvas.offsetTop;
+  // Get the x and y coordinates of the click event
+  const x = e.clientX - imageCanvas.offsetLeft;
+  const y = e.clientY - imageCanvas.offsetTop;
 
-    // Get the pixel color at the specified coordinates
-    const pixelColor = ctx.getImageData(x, y, 1, 1).data;
-
-    // Convert the pixel color to a human-readable color name
-    const color = tinycolor({ r: pixelColor[0], g: pixelColor[1], b: pixelColor[2] });
-    colorOutput.innerHTML = color.toName();
+  // Get the pixel color at the specified coordinates
+  const pixelColor = ctx.getImageData(x, y, 1, 1).data;
+  
+  // Convert the pixel color to a human-readable color name
+  const hex = rgbToHex(pixelColor[0], pixelColor[1], pixelColor[2]);
+  fetch(`https://a645-184-88-51-211.ngrok.io/search/json/?hex=${hex}`)
+  .then(response => response.text())
+  .then(responseText => {
+      try {
+          const data = JSON.parse(responseText);
+          colorOutput.innerHTML = data.name;
+      } catch (error) {
+          console.error(error);
+      }
+  });
+    colorOutput.innerHTML = hex;
 });
+}
