5cb5fdad1fdceebba476f989163ac60b135ecc05
Tristin-Young <86166175+Tristin-Young@users.noreply.github.com>
2023-01-25 02:46:29 -0500

    Updated index JS and css

--- app.js
+++ app.js
@@ -1,53 +1,60 @@
 
-window.onload = function(){
-// Get image input and canvas elements
-const imageInput = document.getElementById('image-input');
-const imageCanvas = document.getElementById('image-canvas');
-const colorOutput = document.getElementById('color-output');
+import getPixels from '../../get-image-pixels';
 
-// Create a canvas context
-const ctx = imageCanvas.getContext('2d');
+window.onload = function() {
+  // Get image input and canvas elements
+  const imageInput = document.getElementById('image-input');
+  const imageCanvas = document.getElementById('image-canvas');
+  const colorOutput = document.getElementById('color-output');
 
-function rgbToHex(r, g, b) {
-  return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
-}
+  // Add event listener to the image input
+  imageInput.addEventListener('change', async (e) => {
+    // Get selected image file
+    const imageFile = e.target.files[0];
 
-// Add event listener to the image input
-imageInput.addEventListener('change', (e) => {
-  // Get selected image file
-  const imageFile = e.target.files[0];
+    // Create an image object
+    const image = new Image();
+    image.src = URL.createObjectURL(imageFile);
 
-  // Create an image object
-  const image = new Image();
-  image.src = URL.createObjectURL(imageFile);
-
-  // Draw image on the canvas
-  image.onload = () => {
-    ctx.drawImage(image, 0, 0, imageCanvas.width, imageCanvas.height);
-  };
-});
-
-// Add event listener to the canvas
-imageCanvas.addEventListener('click', (e) => {
-  // Get the x and y coordinates of the click event
-  const x = e.clientX - imageCanvas.offsetLeft;
-  const y = e.clientY - imageCanvas.offsetTop;
-
-  // Get the pixel color at the specified coordinates
-  const pixelColor = ctx.getImageData(x, y, 1, 1).data;
-  
-  // Convert the pixel color to a human-readable color name
-  const hex = rgbToHex(pixelColor[0], pixelColor[1], pixelColor[2]);
-  fetch(`https://a645-184-88-51-211.ngrok.io/search/json/?hex=${hex}`)
-  .then(response => response.text())
-  .then(responseText => {
-      try {
-          const data = JSON.parse(responseText);
-          colorOutput.innerHTML = data.name;
-      } catch (error) {
-          console.error(error);
-      }
+    // Draw image on the canvas
+    image.onload = async () => {
+      imageCanvas.width = image.width;
+      imageCanvas.height = image.height;
+      const ctx = imageCanvas.getContext('2d');
+      ctx.drawImage(image, 0, 0);
+      const pixels = await getPixels(image);
+    };
   });
-    colorOutput.innerHTML = hex;
-});
-}
+  
+  // Add event listener to the canvas
+  imageCanvas.addEventListener('click', async (e) => {
+    const x = e.clientX - imageCanvas.offsetLeft;
+    const y = e.clientY - imageCanvas.offsetTop;
+
+    // Get the pixel information at the specified coordinates
+    const pixelIndex = (y * imageCanvas.width + x) * 4;
+    const r = pixels.data[pixelIndex];
+    const g = pixels.data[pixelIndex + 1];
+    const b = pixels.data[pixelIndex + 2];
+    const a = pixels.data[pixelIndex + 3];
+
+    console.log(`r: ${r}, g: ${g}, b: ${b}, a: ${a}`);
+  
+    // Convert the pixel color to a human-readable color name
+    const hex = rgbToHex(r, g, b);
+    colorSelected = true;
+    fetch(`https://a645-184-88-51-211.ngrok.io/search/json/?hex=${hex}`, {mode: 'no-cors'})
+    .then(response => response.text())
+    .then(responseText => {
+        try {
+            const data = JSON.parse(responseText);
+            colorOutput.innerHTML = data.name;
+        } catch (error) {
+            console.error(error);
+        }
+    });
+    document.getElementById("hex-output").innerHTML = hex;
+    document.querySelector(".step-3").style.display = "block";
+  });
+  }
+  
